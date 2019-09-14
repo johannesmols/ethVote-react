73e00f8ac72d5f73a453e159ee6033794c6b6140
Johannes Mols <johannes.mols@googlemail.com>
2019-09-15 00:12:43 +0200

    Added react-router that redirects to an error page if metamask isn't installed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import web3 from './web3';
-import registrationAuthority from './ethereum/registrationAuthority';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Web3 from "web3";
+import RegistrationAuthority from "./ethereum/RegistrationAuthority.json";
+import ElectionFactory from "./ethereum/ElectionFactory.json";
+import Home from "./components/Home";
+import Metamask from "./components/Metamask";
+import Error from "./components/Error";
 
 class App extends Component {
+    state = {
+        loading: true
+    };
 
-  async componentDidMount() {
-    const manager = await registrationAuthority.methods.manager().call();
-    console.log(manager);
-  }
+    async componentDidMount() {
+        await this.getWeb3AndContracts();
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+        // to not throw errors in browsers that don't have metamask installed
+        if (this.state.web3) {
+            console.log(
+                "factory manager",
+                await this.state.electionFactory.methods.factoryManager().call()
+            );
+        }
+    }
+
+    async getWeb3AndContracts() {
+        try {
+            await window.web3.currentProvider.enable();
+            this.setState({ web3: new Web3(window.web3.currentProvider) });
+            await this.getRegistrationAuthority();
+            await this.getElectionFactory();
+            this.setState({ loading: false });
+        } catch (err) {
+            this.setState({ loading: false });
+        }
+    }
+
+    async getRegistrationAuthority() {
+        const { web3 } = this.state;
+        const address = "0x74F3F1d24c4bE46e1ef261f48EA87768831cA2C2";
+        const abi = JSON.parse(RegistrationAuthority.interface);
+        const contract = new web3.eth.Contract(abi, address);
+        this.setState({ registrationAuthority: contract });
+    }
+
+    async getElectionFactory() {
+        const { web3 } = this.state;
+        const address = "0xdCaCCc422B7A2d580Ccaa95909b6A9B2E5b0fc05";
+        const abi = JSON.parse(ElectionFactory.interface);
+        const contract = new web3.eth.Contract(abi, address);
+        this.setState({ electionFactory: contract });
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                {this.state.web3 === undefined &&
+                this.state.loading === false ? (
+                    <Redirect to="/metamask" />
+                ) : null}
+                <Switch>
+                    <Route path="/" component={Home} exact />
+                    <Route path="/metamask" component={Metamask} />
+                    <Route component={Error} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Metamask.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Metamask extends Component {
+    state = {};
+    render() {
+        return (
+            <div>
+                <p>Install Metamask</p>
+            </div>
+        );
+    }
+}
+
+export default Metamask;

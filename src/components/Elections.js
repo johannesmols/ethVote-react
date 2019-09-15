c26a71a197b56634a698fc1c8afbbb3b9f5da92f
Johannes Mols <johannes.mols@googlemail.com>
2019-09-15 17:48:58 +0200

    Elections are now loaded successfully (bug that menu has to be clicked once)

--- src/components/Elections.js
+++ src/components/Elections.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-import { Dimmer, Loader, Image, Segment } from "semantic-ui-react";
+import { Menu, Card, Dimmer, Loader, Image, Segment } from "semantic-ui-react";
 import Web3 from "web3";
 import RegistrationAuthority from "../ethereum/RegistrationAuthority.json";
 import ElectionFactory from "../ethereum/ElectionFactory.json";
+import Election from "../ethereum/Election.json";
 
 class Elections extends Component {
     state = {
         loading: true,
-        metamask: false
+        metamask: false,
+        activeItem: "current"
     };
 
     async componentDidMount() {
-        await this.loadBlockchainData();
+        await this.loadWeb3AndContracts();
     }
 
     render() {
@@ -27,7 +29,10 @@ class Elections extends Component {
                         <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
                     </Segment>
                 ) : metamask ? (
-                    <h1>Welcome</h1>
+                    <React.Fragment>
+                        {this.renderMenu()}
+                        {this.renderList()}
+                    </React.Fragment>
                 ) : (
                     <Redirect to="/metamask" />
                 )}
@@ -35,16 +40,79 @@ class Elections extends Component {
         );
     }
 
-    async loadBlockchainData() {
+    renderMenu() {
+        const { activeItem } = this.state;
+        return (
+            <Menu pointing secondary compact widths={"3"}>
+                <Menu.Item
+                    name="past"
+                    active={activeItem === "past"}
+                    onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                    name="current"
+                    active={activeItem === "current"}
+                    onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                    name="upcoming"
+                    active={activeItem === "upcoming"}
+                    onClick={this.handleItemClick}
+                />
+            </Menu>
+        );
+    }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+    renderList() {
+        const { elections } = this.state;
+        const items = elections.map(election => {
+            return {
+                header: election.title,
+                description: election.description,
+                fluid: true
+            };
+        });
+
+        return (
+            <Card.Group
+                items={items}
+                style={{ marginTop: "0.5em", overflow: "hidden" }}
+            />
+        );
+    }
+
+    async loadWeb3AndContracts() {
         try {
             await window.web3.currentProvider.enable();
             this.setState({ web3: new Web3(window.web3.currentProvider) });
             await this.getRegistrationAuthority();
             await this.getElectionFactory();
+            await this.retrieveDeployedElections();
             this.setState({ loading: false, metamask: true });
         } catch (err) {
             this.setState({ loading: false, metamask: false });
         }
+    }
+
+    async retrieveDeployedElections() {
+        const { electionFactory } = this.state;
+        const addresses = await electionFactory.methods
+            .getDeployedElections()
+            .call();
+        let elections = [];
+        addresses.forEach(async e => {
+            const contract = await this.getElectionContract(e);
+            const contractDetails = {
+                title: await contract.methods.title().call(),
+                description: await contract.methods.description().call(),
+                startTime: await contract.methods.startTime().call(),
+                timeLimit: await contract.methods.timeLimit().call()
+            };
+            elections.push(contractDetails);
+        });
+        this.setState({ elections });
     }
 
     async getRegistrationAuthority() {
@@ -61,6 +129,17 @@ class Elections extends Component {
         const abi = JSON.parse(ElectionFactory.interface);
         const contract = new web3.eth.Contract(abi, address);
         this.setState({ electionFactory: contract });
+    }
+
+    async getElectionContract(address) {
+        try {
+            const { web3 } = this.state;
+            const abi = JSON.parse(Election.interface);
+            const contract = new web3.eth.Contract(abi, address);
+            return contract;
+        } catch (err) {
+            console.log(err.message);
+        }
     }
 }
 

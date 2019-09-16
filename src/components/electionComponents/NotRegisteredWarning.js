6d7b595cddb1d238b291c65c89997b3f33b2f58d
Johannes Mols <johannes.mols@googlemail.com>
2019-09-16 15:14:06 +0200

    Made warning stateless component

--- src/components/electionComponents/NotRegisteredWarning.js
+++ src/components/electionComponents/NotRegisteredWarning.js
@@ -1,21 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Message, Button } from "semantic-ui-react";
 
-class NotRegisteredWarning extends Component {
-    render() {
-        return (
-            <Message warning>
-                <Button basic floated="right" as={Link} to="/register">
-                    Get Help
-                </Button>
-                <Message.Header>You are not registered to vote.</Message.Header>
-                <p>
-                    Please go to your local registration authority to register.
-                </p>
-            </Message>
-        );
-    }
+export default function NotRegisteredWarning() {
+    return (
+        <Message warning>
+            <Button basic floated="right" as={Link} to="/register">
+                Get Help
+            </Button>
+            <Message.Header>You are not registered to vote.</Message.Header>
+            <p>Please go to your local registration authority to register.</p>
+        </Message>
+    );
 }
-
-export default NotRegisteredWarning;

960e56ee56c608a0377d6d3c47e68bd11fdec3f9
Johannes Mols <johannes.mols@googlemail.com>
2019-09-27 18:13:36 +0200

    Updated contract ABI's and addresses

--- src/ethereum/addresses.js
+++ src/ethereum/addresses.js
@@ -1,4 +1,4 @@
 module.exports = {
-    registrationAuthority: "0x7CA8bDF1721b332fE1F40260c782f605b37B8BbF",
-    electionFactory: "0x1115b7f57b899651D270470031AC6D6cDEc62364"
+    registrationAuthority: "0xAd735ED077bcfe9EfcAb5816C1F5FeB3D1192e75",
+    electionFactory: "0x0B8450c0774112bfd5Bf417Ebd66b65888eCBBa3"
 };

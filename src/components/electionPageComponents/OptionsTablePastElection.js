9f860eecc87279b4dfd2664282049b1c312a4692
Johannes Mols <johannes.mols@googlemail.com>
2019-11-16 01:32:22 +0100

    Implemented tallying, decryption, and publishing of result

--- src/components/electionPageComponents/OptionsTablePastElection.js
+++ src/components/electionPageComponents/OptionsTablePastElection.js
@@ -19,6 +19,8 @@ class OptionsTablePastElection extends Component {
         modalOpen: false,
         modalState: "",
         errorMessage: "",
+        statusMessage:
+            "This usually takes around 60 seconds. Please stay with us.",
         electionManager: "",
         publishedResults: [],
         privateKey: "",
@@ -58,6 +60,133 @@ class OptionsTablePastElection extends Component {
 
     handleSubmit = async event => {
         event.preventDefault();
+
+        this.setState({ modalOpen: true, modalState: "processing" });
+
+        try {
+            // Create private and public key
+            const privateKeyCredentials = JSON.parse(this.state.privateKey);
+            const privateKey = new paillier.PrivateKey(
+                BigInt(privateKeyCredentials.lambda),
+                BigInt(privateKeyCredentials.mu),
+                BigInt(privateKeyCredentials._p),
+                BigInt(privateKeyCredentials._q),
+                privateKeyCredentials.publicKey
+            );
+            const publicKey = new paillier.PublicKey(
+                BigInt(privateKeyCredentials.publicKey.n),
+                BigInt(privateKeyCredentials.publicKey.g)
+            );
+
+            // Get the list of addresses that voted
+            const voters = await this.props.contract.methods
+                .getListOfAddressesThatVoted()
+                .call();
+
+            if (voters.length === 0) {
+                throw new Error("No votes found.");
+            }
+
+            // Get vote of each voter
+            let encryptedVotes = [];
+            for (let i = 0; i < voters.length; i++) {
+                this.setState({
+                    statusMessage:
+                        "Retrieving encrypted vote " +
+                        (i + 1) +
+                        " of " +
+                        voters.length
+                });
+
+                encryptedVotes.push(
+                    await this.props.contract.methods
+                        .getEncryptedVoteOfVoter(voters[i])
+                        .call()
+                );
+            }
+
+            // Organize encrypted votes in arrays
+            let votesForEachOption = [];
+            for (let i = 0; i < encryptedVotes.length; i++) {
+                votesForEachOption.push(JSON.parse(encryptedVotes[i]));
+            }
+
+            // Tallying encrypted votes
+            let tallyForEachOption = new Array(this.props.options.length);
+
+            for (let i = 0; i < votesForEachOption.length; i++) {
+                this.setState({
+                    statusMessage:
+                        "Tallying encrypted vote " +
+                        (i + 1) +
+                        " of " +
+                        votesForEachOption.length
+                });
+
+                for (let j = 0; j < this.props.options.length; j++) {
+                    if (i === 0) {
+                        tallyForEachOption[j] = BigInt(
+                            votesForEachOption[i][j]
+                        );
+                    } else {
+                        tallyForEachOption[j] = publicKey.addition(
+                            tallyForEachOption[j],
+                            votesForEachOption[i][j]
+                        );
+                    }
+                }
+            }
+
+            // Decrypt tallies
+            let decryptedResultForEachOption = new Array(
+                this.props.options.length
+            );
+
+            for (let i = 0; i < tallyForEachOption.length; i++) {
+                this.setState({
+                    statusMessage:
+                        "Decrypting vote " +
+                        (i + 1) +
+                        " of " +
+                        tallyForEachOption.length
+                });
+
+                decryptedResultForEachOption[i] = privateKey.decrypt(
+                    tallyForEachOption[i]
+                );
+            }
+
+            // Verifying number of votes matches voters
+            const sumOfVotes = decryptedResultForEachOption.reduce(
+                (a, b) => a + b,
+                0
+            );
+
+            if (sumOfVotes !== voters.length) {
+                throw new Error(
+                    "Number of tallied votes is not the same as the number of voters."
+                );
+            }
+
+            // Convert to regular string array
+            let result = new Array(this.props.options.length);
+            for (let i = 0; i < decryptedResultForEachOption.length; i++) {
+                result[i] = decryptedResultForEachOption[i].toString();
+            }
+
+            // Publish results
+            this.setState({
+                statusMessage: "Publishing result"
+            });
+
+            await this.props.contract.methods
+                .publishResults(result)
+                .send({ from: this.props.userAddresses[0] });
+
+            this.setState({ modalState: "success" });
+        } catch (err) {
+            this.setState({ modalState: "error", errorMessage: err.message });
+        }
     };
 
     handleModalClose = () => {
@@ -72,7 +201,7 @@ class OptionsTablePastElection extends Component {
                     modalState={this.state.modalState}
                     handleModalClose={this.handleModalClose}
                     errorMessageDetailed={this.state.errorMessage}
-                    processingMessage="This usually takes around 60 seconds. Please stay with us."
+                    processingMessage={this.state.statusMessage}
                     errorMessage="We encountered an error. Please try again."
                     successMessage="The results have been decrypted and published."
                 />

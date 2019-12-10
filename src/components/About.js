bb5c3bda16bbeb185125639a542d9f874cde26cf
Johannes Mols <johannes.mols@googlemail.com>
2019-12-11 00:57:30 +0100

    Added PDF renderer and abstract to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,52 @@
 import React, { Component } from "react";
+import { Container, Header } from "semantic-ui-react";
+import PDFViewer from "pdf-viewer-reactjs";
 
 class About extends Component {
-    state = {};
     render() {
-        return <h1>About</h1>;
+        return (
+            <Container>
+                <Header as="h1">About this project</Header>
+                <p>
+                    <i>
+                        This project researches possible solutions to Internet
+                        Voting systems that would satisfy the security
+                        requirements of a real election. A solution is proposed
+                        that safely stores and manages votes on the Ethereum
+                        blockchain, encrypts and tallies votes using homomorphic
+                        encryption, and proves the correctness of votes and
+                        final tallies with non-interactive zero-knowledge
+                        proofs. A proof-of-concept is presented that implements
+                        Ethereum smart contracts and a front-end application to
+                        interact with the system. While we believe it is
+                        possible, we were not able to implement zero-knowledge
+                        proofs in this project. We conclude that the proposed
+                        solution is secure, but would likely fail in real-life
+                        due to the trust required by voters, which is unlikely
+                        to be given to a complex system that is hard to
+                        understand without a technical background.
+                    </i>
+                </p>
+                <p>
+                    The project was conducted by Johannes Mols at Aalborg
+                    University in Copenhagen in the context of a semester
+                    project. The project report can be found below or on{" "}
+                    <a
+                        href="https://github.com/johannesmols/ethVote"
+                        target="_blank"
+                    >
+                        GitHub
+                    </a>
+                    , together with the source code.
+                </p>
+                <PDFViewer
+                    document={{
+                        url:
+                            "https://gateway.pinata.cloud/ipfs/QmZoh84kvMWXvwZgu4UdCUhPrxarJKQDDkizqSnNiz74eT"
+                    }}
+                />
+            </Container>
+        );
     }
 }
 

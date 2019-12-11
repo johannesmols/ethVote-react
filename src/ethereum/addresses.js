6caf49f250481437f8963fdfbc2b19204fb9086d
Johannes Mols <johannes.mols@googlemail.com>
2019-12-11 01:13:56 +0100

    Auto stash before revert of "Added PDF renderer and abstract to about page"

--- src/ethereum/addresses.js
+++ src/ethereum/addresses.js
@@ -1,4 +1,4 @@
 module.exports = {
-    registrationAuthority: "0x912a0E44308A03f66Fc5840cba4Ecb81eb7bB82B",
-    electionFactory: "0xD523bde2D06cA779f3438C5C6e590Bc0950931ad"
+    registrationAuthority: "0xFe4052Afe151a3656eE10579f80a8cA60E47a80D",
+    electionFactory: "0xe0532CC114393aA7DFdc2a339BC927C279B80D25"
 };

c4380bbd92846fe4dc4f7e9529ce17d50a2ffbaf
Johannes Mols <johannes.mols@googlemail.com>
2020-01-19 22:06:55 +0100

    Updated addresses

--- src/ethereum/addresses.js
+++ src/ethereum/addresses.js
@@ -1,4 +1,4 @@
 module.exports = {
-    registrationAuthority: "0xFe4052Afe151a3656eE10579f80a8cA60E47a80D",
-    electionFactory: "0xe0532CC114393aA7DFdc2a339BC927C279B80D25"
+    registrationAuthority: "0x3A73a7c6CeEED7F77c97F7f563F9c9716a0fF023",
+    electionFactory: "0xe76D2974606dCabA7B1538bB9998476B04cD0087"
 };
